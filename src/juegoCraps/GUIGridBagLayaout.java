ba2f5bf1a333ff6969ea0c0fb7dea2bc29d0f157
2040104 Juan Pablo Pantoja <juan.pablo.pantoja@correounivalle.edu.co>
2021-12-07 20:24:21 -0500

    GridBagLayout

---
+++ src/juegoCraps/GUIGridBagLayaout.java
@@ -0,0 +1,97 @@
+package juegoCraps;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUIGridBagLayaout extends JFrame {
+    private static final String MENSAJE_INICIO="Bienvenido a Craps \n"
+            + "Oprime el botón lanzar para inicar el juego"
+            + "\nSi tu tiro de salida es 7 u 11 ganas con Natural"
+            + "\nSi tu tiro de salida es 2, 3 u 12 pierdes con Craps"
+            + "\nSi sacas cualquier otro valor establecerás el punto"
+            + "\nEstando en punto podrás seguir lanzando los dados"
+            + "\npero ahora ganarás si sacas nuevamente el valor del Punto "
+            + "\nsin que previamente hayas sacado 7";
+
+    private Header headerProject;
+    private JLabel dado1, dado2;
+    private JButton lanzar, ayuda, salir;
+    private JPanel panelDados;
+    private ImageIcon imageDado;
+    private JTextArea resultadosDados, mensajeSalida;
+    private Escucha escucha;
+    private ModelCraps modelCraps;
+
+    public GUIGridBagLayaout(){
+        initGUI();
+
+        //Default JFrame configuration
+        this.setTitle("Juego Craps");
+        this.pack();
+        this.setResizable(true);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void initGUI() {
+        //Set up JFrame Container's Layout
+        this.getContentPane().setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+
+        //Create Listener Object or Control Object
+        escucha = new Escucha();
+        modelCraps = new ModelCraps();
+        //Set up JComponents
+        headerProject = new Header("Mesa juego Craps", Color.BLACK);
+        constraints.gridx=0;
+        constraints.gridy=0;
+        constraints.gridwidth=2;
+        constraints.fill=GridBagConstraints.HORIZONTAL;
+        this.add(headerProject,constraints);
+
+        ayuda = new JButton("?");
+        ayuda.addActionListener(escucha);
+        constraints.gridx=0;
+        constraints.gridy=1;
+        constraints.gridwidth=1;
+        constraints.fill=GridBagConstraints.NONE;
+        constraints.anchor=GridBagConstraints.LINE_START;
+        this.add(ayuda,constraints);
+
+        salir = new JButton("Salir");
+        salir.addActionListener(escucha);
+        constraints.gridx=1;
+        constraints.gridy=1;
+        constraints.gridwidth=1;
+        constraints.fill=GridBagConstraints.NONE;
+        constraints.anchor=GridBagConstraints.LINE_END;
+        this.add(salir,constraints);
+    }
+
+    public static void main(String[] args){
+        EventQueue.invokeLater(() -> {
+            GridBagLayout miProjectGUI = new GridBagLayout();
+        });
+    }
+
+    private class Escucha implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            modelCraps.calcularTiro();
+            int[] caras = modelCraps.getCaras();
+            imageDado = new ImageIcon(getClass().getResource("/recursos/"+caras[0]+".png"));
+            dado1.setIcon(imageDado);
+            imageDado = new ImageIcon(getClass().getResource("/recursos/"+caras[1]+".png"));
+            dado2.setIcon(imageDado);
+            modelCraps.determinarJuego();
+
+            resultadosDados.setText(modelCraps.getEstadoToString()[0]);
+            mensajeSalida.setRows(4);
+            mensajeSalida.setText(modelCraps.getEstadoToString()[1]);
+        }
+    }
+}
